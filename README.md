44a5081754e9ebd50b1dab83bfe17b3aafe330e8
Oliver Snell <oliver@snell.me>
2016-08-16 00:08:04 -0400

    initial commit

---
+++ README.md
@@ -0,0 +1,11 @@
+## Magic Eight Ball
+
+Ever wanted to forgo all responsibility for life's small decisions - then this Alexa skill is for you!  Simply ask Magic Eight Ball about some of life's big questions and she wi
+
+## Code Example
+
+Create a new AWS Lambda node.js function and then upload these contents as a zip.  Then connect with the front end Alexa skill using the ARN.
+
+## Motivation
+
+To learn the Alexa toolkit and begin to imagine what is possible.
---
+++ index.js
@@ -0,0 +1,68 @@
+'use strict';
+var Alexa = require('alexa-sdk');
+
+var APP_ID = undefined; //OPTIONAL: replace with "amzn1.echo-sdk-ams.app.[your-unique-value-here]";
+var SKILL_NAME = 'Unofficial Magic Eight Ball';
+
+/**
+ * Array containing space facts.
+ */
+var FACTS = [
+  "It is certain",
+  "It is decidedly so",
+  "Without a doubt",
+  "Yes, definitely",
+  "You may rely on it",
+  "As I see it, yes",
+  "Most likely",
+  "Outlook good",
+  "Yes",
+  "Signs point to yes",
+  "Reply hazy try again",
+  "Ask again later",
+  "Better not tell you now",
+  "Cannot predict now",
+  "Concentrate and ask again",
+  "Don't count on it",
+  "My reply is no",
+  "My sources say no",
+  "Outlook not so good",
+  "Very doubtful"
+];
+
+exports.handler = function(event, context, callback) {
+    var alexa = Alexa.handler(event, context);
+    alexa.APP_ID = APP_ID;
+    alexa.registerHandlers(handlers);
+    alexa.execute();
+};
+
+var handlers = {
+    'LaunchRequest': function () {
+        this.emit('AMAZON.HelpIntent');
+    },
+    'GetFortuneIntent': function () {
+        this.emit('GetFortune');
+    },
+    'GetFortune': function () {
+        // Get a random space fact from the space facts list
+        var factIndex = Math.floor(Math.random() * FACTS.length);
+        var randomFact = FACTS[factIndex];
+
+        // Create speech output
+        var speechOutput = randomFact;
+
+        this.emit(':tellWithCard', speechOutput, SKILL_NAME, randomFact)
+    },
+    'AMAZON.HelpIntent': function () {
+        var speechOutput = "You can ask me whether you should do something or if something will happen... What can I help you with?";
+        var reprompt = "What can I help you with?";
+        this.emit(':ask', speechOutput, reprompt);
+    },
+    'AMAZON.CancelIntent': function () {
+        this.emit(':tell', 'Goodbye!');
+    },
+    'AMAZON.StopIntent': function () {
+        this.emit(':tell', 'Goodbye!');
+    }
+};
